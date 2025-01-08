041c8b09baa9c855b5b10deb9b727aa0948a902b
Isa <a1989@josaff.local>
2025-01-08 23:52:41 +0600

    fix images on false and  footer links

--- src/app/home/ui/News/NewsCard/NewsCard.tsx
+++ src/app/home/ui/News/NewsCard/NewsCard.tsx
@@ -3,24 +3,24 @@ import styles from "./NewsCard.module.scss";
 import { FC } from "react";
 
 interface INewsCard {
-  image: string;
+  image?: string;
   title: string;
   text: string;
 }
 
-export const NewsCard:FC<INewsCard> = ({image, title, text}) => {
+export const NewsCard: FC<INewsCard> = ({ image, title, text }) => {
   return (
     <li className={styles.item}>
       <div className={styles.imgWrapper}>
-        <Image src={image} alt="news image" width={400} height={245} />
+        {image ? (
+          <Image src={image} alt="news image" width={400} height={245} />
+        ) : (
+          <div className={styles.noImage}>Нет данных</div>
+        )}
       </div>
       <div className={styles.inner}>
-        <h4 className={styles.innerTitle}>
-          {title}
-        </h4>
-        <p className={styles.text}>
-          {text}
-        </p>
+        <h4 className={styles.innerTitle}>{title}</h4>
+        <p className={styles.text}>{text}</p>
       </div>
     </li>
   );
