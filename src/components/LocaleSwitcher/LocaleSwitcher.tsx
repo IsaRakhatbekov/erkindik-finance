5531b5aff7bdaa0835d481c8c2ca5b46580e26cb
Mikhaylov-M <mikhaylov.m@outlook.com>
2025-03-19 00:57:13 +0600

    change flags

--- src/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,23 +1,64 @@
-import { useLocale } from "next-intl";
-import { routing } from "@/src/i18n/routing";
-import LocaleSwitcherSelect from "../LocaleSwitcherSelect/LocaleSwitcherSelect";
+"use client";
 
-const localeFlags: Record<string, string> = {
-  ru: "🇷🇺",
-  ky: "🇰🇬",
-  en: "🇺🇸",
+import { useState, useTransition } from "react";
+import { useRouter, usePathname, Locale } from "@/src/i18n/routing";
+import styles from "./LocaleSwitcher.module.scss";
+import Image from "next/image";
+import ruFlag from "@/public/flags/ru.svg";
+import kyFlag from "@/public/flags/ky.svg";
+import usFlag from "@/public/flags/en.svg";
+import { useLocale } from "next-intl";
+
+const localeFlagImages: Record<string, string> = {
+  ru: ruFlag.src,
+  ky: kyFlag.src,
+  en: usFlag.src,
 };
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+
+  function handleLocaleChange(newLocale: Locale) {
+    if (newLocale === locale) return;
+    setIsOpen(false);
+
+    startTransition(() => {
+      router.replace({ pathname }, { locale: newLocale });
+    });
+  }
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale}>
-      {routing.locales.map((cur) => (
-        <option key={cur} value={cur}>
-          {localeFlags[cur] || cur.toUpperCase()}
-        </option>
-      ))}
-    </LocaleSwitcherSelect>
+    <div className={styles.localeWrapper}>
+      <div
+        className={styles.selected}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <Image
+          src={localeFlagImages[locale]}
+          alt={locale}
+          width={30}
+          height={30}
+        />
+      </div>
+
+      {isOpen && (
+        <ul className={styles.dropdown}>
+          {Object.keys(localeFlagImages).map((cur) => (
+            <li key={cur} onClick={() => handleLocaleChange(cur as Locale)}>
+              <Image
+                src={localeFlagImages[cur]}
+                alt={cur}
+                width={30}
+                height={30}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
